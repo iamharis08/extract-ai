51893ce15d2a7c72ff05161670579f816ce31a7d
Haris Ahmed <iamharis@Hariss-MacBook-Air.local>
2025-08-15 01:33:20 -0400

    fixed local testing bugs

--- processor-function/main.py
+++ processor-function/main.py
@@ -6,17 +6,19 @@ from google.cloud import documentai_v1 as documentai
 from google.cloud import bigquery
 from google.cloud import pubsub_v1
 
+
 # Load environment variables for local development
 load_dotenv()
 
 # --- CONFIGURATION ---
 PROJECT_ID = os.getenv("PROJECT_ID") 
 LOCATION = os.getenv("LOCATION")
+# AI_LOCATION = os.getenv("AI_LOCATION")
 PROCESSOR_ID = os.getenv("PROCESSOR_ID")
 
 # --- Clients are initialized globally for reuse ---
 docai_client = documentai.DocumentProcessorServiceClient()
-bq_client = bigquery.Client()
+bq_client = bigquery.Client(project=PROJECT_ID)
 publisher = pubsub_v1.PublisherClient()
 
 
@@ -25,6 +27,8 @@ def process_invoice(event, context):
     Cloud Function triggered by a Pub/Sub message when a new invoice is uploaded.
     It processes the document using Document AI and stores the result in BigQuery.
     """
+
+    # This section is for the pub/sub and exctracting data from it ---------------------/
     # Decode the Pub/Sub message to get file details
     pubsub_message_data = base64.b64decode(event['data']).decode('utf-8')
     file_data = json.loads(pubsub_message_data)
@@ -40,13 +44,21 @@ def process_invoice(event, context):
 
     user_id, filename = parts
     print(f"Processing '{filename}' for user '{user_id}'.")
-
-    # Call the Document AI API to process the invoice
+   #----------------------------------------------------------------------------
+    #Function
+    #grabs the google cloud storage url path for uploaded invoice
     gcs_uri = f"gs://{bucket_name}/{full_path}"
     processor_name = docai_client.processor_path(PROJECT_ID, LOCATION, PROCESSOR_ID)
     
-    raw_document = documentai.RawDocument(gcs_uri=gcs_uri, mime_type="application/pdf")
-    request = documentai.ProcessRequest(name=processor_name, raw_document=raw_document)
+     # then calls the Document AI API to process the invoice
+    gcs_document = documentai.GcsDocument(
+        gcs_uri=gcs_uri, 
+        mime_type="application/pdf"
+        )
+    request = documentai.ProcessRequest(
+        name=processor_name,
+        gcs_document=gcs_document
+        )
     
     result = docai_client.process_document(request=request)
     document = result.document
@@ -79,7 +91,7 @@ def process_invoice(event, context):
 
     # Insert the row into the BigQuery table
     table_id = f"{PROJECT_ID}.invoice_processing.processed_invoices"
-    
+
     try:
         errors = bq_client.insert_rows_json(table_id, [row_to_insert])
         if not errors:
